528afa24bbb718241cf07882c39cf9665293ed2d
kes20 <ke.s2013@yandex.ru>
2023-06-21 14:03:19 +0300

    fixed two method

---
+++ homework2/method_two/src/main/java/ru/sber/conf/ConfigApp.java
@@ -0,0 +1,12 @@
+package ru.sber.conf;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ru.sber.models")
+public class ConfigApp {
+
+
+}
---
+++ homework2/method_two/src/main/java/ru/sber/models/Parrot.java
@@ -0,0 +1,9 @@
+package ru.sber.models;
+
+
+public interface Parrot {
+
+    public void say();
+    public String getName();
+    public void setName(String name);
+}
