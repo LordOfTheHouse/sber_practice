d1609ed94d7a1ca6983e8e6dc9dc2badb33649eb
kes20 <ke.s2013@yandex.ru>
2023-07-05 10:55:53 +0300

    homework12

--- homework12/src/components/AutoForm.js
+++ homework12/src/components/AutoForm.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { Card, Button, Input } from 'antd';
-import { useDispatch, useSelector } from "react-redux";
-import { signIn, registration } from "../slices/UserSlice";
-import { RegistrationForm } from "./Registration";
+import React, {useState} from 'react';
+import {Card, Button, Input} from 'antd';
+import {useDispatch, useSelector} from "react-redux";
+import {signIn, registration} from "../slices/UserSlice";
+import {newCart} from "../slices/CartSlice";
+import {RegistrationForm} from "./Registration";
 
 export const AuthForm = () => {
-    const isReg = useSelector((state) => state.user.isReg);
+    const isRegistrationForm = useSelector((state) => state.user.isReg);
+    const currUser = useSelector((state) => state.user.user);
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const dispatch = useDispatch();
@@ -23,8 +25,8 @@ export const AuthForm = () => {
 
     return (
         <>
-            {isReg ? (
-                <RegistrationForm />
+            {isRegistrationForm ? (
+                <RegistrationForm/>
             ) : (
                 <Card
                     hoverable
@@ -36,15 +38,15 @@ export const AuthForm = () => {
                         placeholder="Email"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
-                        style={{ marginBottom: '10px' }}
+                        style={{marginBottom: '10px'}}
                     />
                     <Input.Password
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        style={{ marginBottom: '10px' }}
+                        style={{marginBottom: '10px'}}
                     />
-                    <Button type="primary" style={{ marginBottom: '10px' }} onClick={handleLogin}>
+                    <Button type="primary" style={{marginBottom: '10px'}} onClick={handleLogin}>
                         Войти
                     </Button>
                     <Button onClick={handleRegister}>Зарегистрироваться</Button>
--- homework12/src/components/Registration.js
+++ homework12/src/components/Registration.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { Card, Button, Input } from 'antd';
-import { useDispatch } from "react-redux";
-import { createUser } from "../slices/UserSlice";
+import React, {useState} from 'react';
+import {Card, Button, Input} from 'antd';
+import {useDispatch} from "react-redux";
+import {createUser} from "../slices/UserSlice";
 
 export const RegistrationForm = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const dispatch = useDispatch();
-    const createUs = () => {
+    const createAccount = () => {
         dispatch(createUser({
-            name:name,
-            email:email,
-            password:password
+            name: name,
+            email: email,
+            password: password
         }))
     };
 
@@ -27,21 +27,21 @@ export const RegistrationForm = () => {
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                style={{ marginBottom: '10px' }}
+                style={{marginBottom: '10px'}}
             />
             <Input
                 placeholder="Имя"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                style={{ marginBottom: '10px' }}
+                style={{marginBottom: '10px'}}
             />
             <Input.Password
                 placeholder="Пароль"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                style={{ marginBottom: '10px' }}
+                style={{marginBottom: '10px'}}
             />
-            <Button type="primary" style={{ marginBottom: '10px' }} onClick={createUs}>
+            <Button type="primary" style={{marginBottom: '10px'}} onClick={createAccount}>
                 Зарегистрироваться
             </Button>
         </Card>
